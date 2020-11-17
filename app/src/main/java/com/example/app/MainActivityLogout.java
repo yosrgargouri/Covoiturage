bb669ca07791621a3c39117e4c5c14c3aa48ced0
yosr <yosr.gargouri@supcom.tn>
2020-11-17 22:13:59 +0100

    init project

---
+++ app/src/main/java/com/example/app/MainActivityLogout.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLogout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_logout);
+    }
+}
